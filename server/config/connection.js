8e5b63ec43497cdec136b0f84227979c98804004
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2022-01-05 19:54:53 -0700

    fixing connections

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://conleymarie:Terrific23!@cluster0.ldluu.mongodb.net/peaceful-reaches-75061?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongoDB://localhost/mern-challenge', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
